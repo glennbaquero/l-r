4fa9b854a347ed58562f1008276a80361d984aa0
baqueroglenncarlo <baqueroglenncarlo@gmail.com>
2021-08-03 15:35:17 +0800

    Fix issue in buttons

--- resources/views/pages/group-message/index.blade.php
+++ resources/views/pages/group-message/index.blade.php
@@ -4,6 +4,13 @@
             <div class="text-base mr-auto">
                 <span class="font-semibold mr-3">{{__('Group Message Management')}}</span>
             </div>
+            {{-- <div class="flex text-base items-center">
+                <a href="{{ route('group-message.create') }}" class="inline-flex font-normal items-center px-4 py-2 border border-transparent text-base leading-4 font-medium rounded-md text-white bg-lightblue hover:bg-lighterblue focus:outline-none focus:border-lighterblue focus:shadow-outline-lighterblue active:bg-lighterblue transition ease-in-out duration-150">
+                    {{__('New Group Message')}}
+                </a>
+            </div> --}}
+        </div>
+        <div class="flex items-center mt-4">
             <div class="flex text-base items-center">
                 <a href="{{ route('group-message.create') }}" class="inline-flex font-normal items-center px-4 py-2 border border-transparent text-base leading-4 font-medium rounded-md text-white bg-lightblue hover:bg-lighterblue focus:outline-none focus:border-lighterblue focus:shadow-outline-lighterblue active:bg-lighterblue transition ease-in-out duration-150">
                     {{__('New Group Message')}}
--- resources/views/pages/price/index.blade.php
+++ resources/views/pages/price/index.blade.php
@@ -4,6 +4,16 @@
             <div class="text-base mr-auto">
                 <span class="font-semibold mr-3">{{__('Price Management')}}</span>
             </div>
+            {{-- <div class="flex text-base items-center">
+                <a href="{{ route('price.create') }}" class="inline-flex font-normal items-center px-4 py-2 border border-transparent text-base leading-4 font-medium rounded-md text-white bg-lightblue hover:bg-lighterblue focus:outline-none focus:border-lighterblue focus:shadow-outline-lighterblue active:bg-lighterblue transition ease-in-out duration-150">
+                    {{__('New Price')}}
+                </a> &nbsp;
+                <a href="{{ route('city.upload') }}" class="inline-flex font-normal items-center px-4 py-2 border border-transparent text-base leading-4 font-medium rounded-md text-white bg-lightblue hover:bg-lighterblue focus:outline-none focus:border-lighterblue focus:shadow-outline-lighterblue active:bg-lighterblue transition ease-in-out duration-150">
+                    {{__('Batch Upload')}}
+                </a>
+            </div> --}}
+        </div>
+        <div class="flex items-center mt-4">
             <div class="flex text-base items-center">
                 <a href="{{ route('price.create') }}" class="inline-flex font-normal items-center px-4 py-2 border border-transparent text-base leading-4 font-medium rounded-md text-white bg-lightblue hover:bg-lighterblue focus:outline-none focus:border-lighterblue focus:shadow-outline-lighterblue active:bg-lighterblue transition ease-in-out duration-150">
                     {{__('New Price')}}
